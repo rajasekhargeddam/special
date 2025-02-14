19bffc296e68f99335b3abfa16373931e3a8fa46
rajasekhargeddam <153250480+rajasekhargeddam@users.noreply.github.com>
2025-02-14 11:13:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,21 @@ const wrapper = document.querySelector('.wrapper');
 const question = document.querySelector('.question');
 const yesBtn = document.querySelector('.yes-btn');
 const noBtn = document.querySelector('.no-btn');
-const wrapperRect = wrapper.getBoundingClientRect();
-const noBtnRect = noBtn.getBoundingClientRect();
+
 yesBtn.addEventListener('click', () => {
-    question.innerHTML = 'OMG thank you sunshine :)';
+    question.innerHTML = 'OMG, thank you sunshine! 😍💕';
+    yesBtn.style.display = 'none';
+    noBtn.style.display = 'none';
 });
+
 noBtn.addEventListener('mouseover', () => {
-    const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
-    const j = Math.floor(Math.random() * (wrapperRect.height - noBtnRect.height)) + 1;
-    noBtn.style.left = i + 'px';
-    noBtn.style.top = j + 'px';
+    const maxX = wrapper.clientWidth - noBtn.clientWidth;
+    const maxY = wrapper.clientHeight - noBtn.clientHeight;
+
+    const randomX = Math.random() * maxX;
+    const randomY = Math.random() * maxY;
+
+    noBtn.style.position = "absolute";
+    noBtn.style.left = `${randomX}px`;
+    noBtn.style.top = `${randomY}px`;
 });
